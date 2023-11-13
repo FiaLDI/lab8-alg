1949642118c0508b80a2a10114b76f63503ae510
FiaLDI <leon.youtube2017@yandex.ru>
2023-11-13 09:40:56 +0300

    merge_sort

---
+++ prog/merge_sort.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from collections import deque
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr, 0
+    else:
+        m = len(arr) // 2
+        left, inversion_left = merge_sort(arr[:m])
+        right, inversion_right = merge_sort(arr[m:])
+        merged, inversion_merge = merge(deque(left), deque(right))
+        return merged, inversion_left + inversion_right + inversion_merge
+
+
+def merge(left, right):
+    merged = []
+    inversion = 0
+    while left and right:
+        if left[0] <= right[0]:
+            merged.append(left.popleft())
+        else:
+            merged.append(right.popleft())
+            inversion += len(left)
+    merged.extend(left)
+    merged.extend(right)
+    return merged, inversion
+
+def main ():
+    array = [8, 7, 3, 10, 1, 6, 4, 1, 9, 2, 11]
+    print("Array =", array)
+    _, inversion = merge_sort(array)
+    print("Количество инверсий в массиве =", inversion)
+
+
+if __name__ == '__main__':
+    main()
